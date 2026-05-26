8856655cdc2e587ef7c71a67725cebab5e3ac7de
khaingth4557-bit <khaingth4557@gmail.com>
2026-05-26 19:40:54 +0630

    onion.py

--- onion.py
+++ onion.py
@@ -105,7 +105,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
 def main() -> None:
     # အစ်ကို့ Telegram Bot Token
-    application = Application.builder().token("8073390602:AAEwUxo9_sVTbW61udQ1ohBMg9eI5PGoceU").build()
+    application = Application.builder().token("8733906002:AAEWuxo9_sVTbW61udQ1ohBMq9eI5PGoceU").build()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
